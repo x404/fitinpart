f4ef872709c050522b11699418555f1f06ddf3ef
Aleksey <kronos2003@gmail.com>
2019-09-17 15:28:32 +0300

    Added "part number-part size" searchbox on the homepage.

--- gulpfile.js
+++ gulpfile.js
@@ -79,6 +79,7 @@ gulp.task('compress', function(){
 				config.templateDir + '/js/modernizr.js',
 				config.libsDir + '/jquery/dist/jquery.js',
 				config.templateDir + '/js/util.js',
+				config.templateDir + '/js/tab.js', 
 				// config.templateDir + '/js/collapse.js',
 				config.templateDir + '/js/popper.min.js',
 				config.templateDir + '/js/dropdown.js',
