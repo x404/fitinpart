ab7052a1fccf5ffe3ca8cc2d2c9a1877b6e750ff
Aleksey <kronos2003@gmail.com>
2019-11-05 12:57:10 +0200

    Responsive Catalog page

--- app/template/js/engine.js
+++ app/template/js/engine.js
@@ -1,3 +1,7 @@
+var isSafari = /constructor/i.test(window.HTMLElement) || (function (p) { return p.toString() === "[object SafariRemoteNotification]"; })(!window['safari'] || safari.pushNotification);
+if (isSafari) document.querySelector('body').classList.add('safari');
+
+
 $(document).ready(function(){
 
 
