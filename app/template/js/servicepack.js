525aa7f5b9f0c38fb616d390a9492e2dd620e56f
Aleksey <kronos2003@gmail.com>
2019-11-07 19:09:21 +0200

    js - add/change items to service package

--- app/template/js/servicepack.js
+++ app/template/js/servicepack.js
@@ -20,10 +20,10 @@ $(document).ready(function(){
 		});
 	});
 
-	//remove item from package
+	//REMOVE ITEM FROM PACKAGE
 	$(document).on('click','.consist .btn-delete', function(e){
 		e.preventDefault();
-		const parentcat = $(this).data('parent-cat');
+		const parentcat = $(this).parent().data('parent-cat');
 
 		// remove green border and change del button to cart-button in item-product list
 		delItemFromPack(parentcat);
@@ -36,36 +36,70 @@ $(document).ready(function(){
 	})
 
 	function delItemFromPack(parentcat){
-		let $inpack = $('.products__list .item-products.inpack');
+		let $currentPack = $('.products__list .item-products.inpack');
 
 		// return "add to pack" button in box package
-		$('.servicepack .btn[data-parent-cat="' + parentcat + '"]').closest('.consist__field').html('<button type="button" class="btn btn-addtopack btn-inline"data-parent-cat="' + parentcat + '">Add to pack</button>');
+		$('.servicepack .consist__field[data-parent-cat="' + parentcat + '"]').html('<button type="button" class="btn btn-addtopack btn-inline">Add to pack</button>');
 
 		// item-product
-		$inpack.find('.btn-delfrompack').addClass('hide');
-		$inpack.find('.btn-cart').removeClass('hide');
+		$currentPack.find('.btn-delfrompack').addClass('hide');
+		$currentPack.find('.btn-addtopack').removeClass('hide');
 
 		// change line in box package
-		$inpack.removeClass('inpack');		
+		$currentPack.removeClass('inpack');		
 	}
 
 
 
-	// add to pack
-	$(document).on('click','.btn-addtopack', function(e){
+	// ADD TO PACK
+	// add to pack from box serviceback
+	$(document).on('click','.servicepack .btn-addtopack', function(e){
 		e.preventDefault();
 		let $this = $(this),
-			$parentcat = $this.data('parent-cat');
+			$parentcat = $this.parent().data('parent-cat');
 
 		$('.products__list .item-products.active').removeClass('active');
-		
 
-		$('.products__list .item-products[data-svc_pattern_value=' + $parentcat + ']').addClass('active');
-        let first_el=$('.products__list .item-products.active').first();
-        if(first_el.length) {
-            $('html, body').animate({
-                scrollTop: first_el.offset().top - 20
-            }, 300);
-        }
+		// activated products
+		$('.item-products').addClass('hide');
+		$('.products__list .item-products[data-svc_pattern_value=' + $parentcat + ']').addClass('active').removeClass('hide');
+		let first_el = $('.products__list .item-products.active').first();
+		if(first_el.length) {
+			$('html, body').animate({
+				scrollTop: first_el.offset().top - 20
+			}, 300);
+		};
+
+		//  hide buttons "to cart"
+		$('.item-products.active').find('.btn-cart, .btn-notify').addClass('hide');
+
+		// show button "to package"
+		$('.item-products.active .btn-addtopack').removeClass('hide');
+	});
+
+
+	// add to pack from list products
+	$(document).on('click','.item-products.active .btn-addtopack', function(e){
+		e.preventDefault();
+		let $this = $(this),
+			$itemproducts = $this.closest('.item-products'),
+			$title = $this.data('title'),
+			$currentcat = $itemproducts.data('svc_pattern_value'),
+			$oldItem = $('.item-products.inpack');
+
+		// remove border from active product
+		$oldItem.removeClass('inpack');
+		$oldItem.find('.btn-delfrompack').addClass('hide');
+		$oldItem.find('.btn-addtopack').removeClass('hide');
+
+		// add Class
+		$itemproducts.addClass('inpack');
+
+		// change button to 'delfrompack'
+		$this.addClass('hide');
+		$itemproducts.find('.btn-delfrompack').removeClass('hide');
+
+		// update title
+		$('[data-parent-cat=' + $currentcat + ']').html($title + '<button type="button" class="btn btn-delete" aria-label="Delete position"></button>')
 	});
 });
